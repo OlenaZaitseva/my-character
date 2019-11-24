314104ff03f2a271b15780ebda4102d299e5164c
Olena Zaitseva <olena@messagebird.com>
2019-11-24 15:27:55 +0100

    Updates

---
+++ precache-manifest.f776552471fbf47cbdb155f766a3d4ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a9e07e7273485d1cedd1e3b0e31d78",
+    "url": "/my-character/index.html"
+  },
+  {
+    "revision": "f460c515cb80399bff1f",
+    "url": "/my-character/static/css/main.a71672f3.chunk.css"
+  },
+  {
+    "revision": "a018a15ba60cd8b3af32",
+    "url": "/my-character/static/js/2.0973e7e4.chunk.js"
+  },
+  {
+    "revision": "f460c515cb80399bff1f",
+    "url": "/my-character/static/js/main.31a35fa6.chunk.js"
+  },
+  {
+    "revision": "90321a5b66fb67ed4fc4",
+    "url": "/my-character/static/js/runtime-main.7881170c.js"
+  }
+]);
